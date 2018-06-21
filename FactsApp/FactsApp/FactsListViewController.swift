e2d60004e8cec65db9932d24e0e26efafe95245c
RajeshBalli <rajesh.balli@cognizant.com>
2018-06-21 11:47:02 +0530

    Renamed ViewController.swift to FactsListViewController.swift and ViewController class to FactsListViewController class.

---
+++ FactsApp/FactsApp/FactsListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FactsListViewController.swift
+//  FactsApp
+//
+//  Created by cts on 21/06/18.
+//  Copyright © 2018 CTS. All rights reserved.
+//
+
+import UIKit
+
+class FactsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "Testing"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
