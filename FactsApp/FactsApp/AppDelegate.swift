69c1ef794edea0f3230aaf008431fc78dd2a2e32
RajeshBalli <rajesh.balli@cognizant.com>
2018-06-21 11:49:31 +0530

    Create UIWindow programatically to host FactsListViewController within a navigation controller.

--- FactsApp/FactsApp/AppDelegate.swift
+++ FactsApp/FactsApp/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let viewController = FactsListViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window?.rootViewController = navigationController
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+
         return true
     }
 
